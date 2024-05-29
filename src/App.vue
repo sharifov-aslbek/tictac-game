2d0346b7a5a80e5312db0a65f9c14a865baabdda
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-05-29 15:08:58 +0500

    First commit on tictac toe game vue

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+
+<script setup>
+  import {ref , computed} from 'vue'
+
+  const player = ref('X');
+  const board = ref([
+    ['','',''],
+    ['','',''],
+    ['','',''],
+  ])
+
+  const CalculateWinner = (board) => {
+  const lines = [
+    [0, 1, 2],[3, 4, 5],
+    [6, 7, 8],[0, 3, 6],
+    [1, 4, 7],[2, 5, 8],
+    [0, 4, 8],[2, 4, 6]
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a]
+    }
+  }
+
+  return null;
+}
+
+const winner  = computed(() => CalculateWinner(board.value.flat()))
+
+const makeMove = (x , y) => {
+  if (winner.value) return
+
+  if(board.value[x][y] !== '') return
+
+  board.value[x][y] = player.value
+  player.value = player.value === 'X' ? 'O' : 'X'
+}
+
+const resetGame = () => {
+  
+}
+</script>
+
+<template>
+  <h1>hello world</h1>
+</template>
+
+
+<style scoped>
+
+</style>
